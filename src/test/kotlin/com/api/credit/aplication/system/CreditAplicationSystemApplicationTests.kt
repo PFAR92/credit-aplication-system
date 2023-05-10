b7357c0e12f6d487f8159c02583718eb4487158f
Paulo Felipe <paulofelipetj@hotmail.com>
2023-05-10 16:52:57 -0300

    first commit

---
+++ src/test/kotlin/com/api/credit/aplication/system/CreditAplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.credit.aplication.system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditAplicationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
