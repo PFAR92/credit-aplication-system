76d54cf1e77a76e60483dbea0d5a2bcda7266c14
Paulo Felipe <paulofelipetj@hotmail.com>
2023-05-23 18:07:54 -0300

    teste Customer save

--- src/test/kotlin/com/api/credit/aplication/system/CustomerServiceTest.kt
+++ src/test/kotlin/com/api/credit/aplication/system/CustomerServiceTest.kt
@@ -1,10 +1,64 @@
 package com.api.credit.aplication.system
 
+import com.api.credit.aplication.system.entity.Address
+import com.api.credit.aplication.system.entity.Customer
+import com.api.credit.aplication.system.repository.CustomerRepository
+import com.api.credit.aplication.system.service.impl.CustomerService
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.test.context.ActiveProfiles
+import java.math.BigDecimal
 
 @ActiveProfiles("test")
 @ExtendWith(MockKExtension::class)
 class CustomerServiceTest {
+
+    @MockK lateinit var customerRepository: CustomerRepository
+    @InjectMockKs lateinit var customerService: CustomerService
+
+    @Test
+    fun `should create customer`(){
+        //given
+        val fakeCustomer: Customer = buildCustomer()
+        every { customerRepository.save(any())} returns fakeCustomer
+
+        //when
+        val actual = customerService.save(fakeCustomer)
+
+        //then
+        Assertions.assertThat(actual).isNotNull
+        Assertions.assertThat(actual).isSameAs(fakeCustomer)
+        verify(exactly = 1) { customerRepository.save(fakeCustomer) }
+    }
+
+    private fun buildCustomer(
+        firstName: String = "Paulo",
+        lastName: String = "Felipe",
+        cpf: String = "28475934625",
+        email: String = "paulo@gmail.com",
+        password: String = "123456",
+        zipCode: String = "38740000",
+        street: String = "Rua da Jujuba",
+        income: BigDecimal = BigDecimal.valueOf(1000.0),
+        id: Long = 1L
+    ) = Customer(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        password = password,
+        address = Address(
+            zipCode = zipCode,
+            street = street,
+        ),
+        income = income,
+        id = id
+    )
+
 }
